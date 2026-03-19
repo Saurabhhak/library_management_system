3556a27c2a7ccef28c0e919ed4ecf38ee5263940
Saurabh <skkashap2328@email.com>
2026-03-19 10:39:06 +0530

    feat(backend/admin-routes): secure admin routes with role-based access control
    - Protected admin routes using authMiddleware
    - Restricted critical operations with superAdminMiddleware
    - Added profile and self account deletion endpoints
    - Ensured secure CRUD operations for admin management

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -1,8 +1,6 @@
-// routes/admin.routes.js
 const express = require("express");
 const router = express.Router();
 
-// -------- Controllers --------
 const {
   createAdmin,
   getAllAdmins,
@@ -10,51 +8,31 @@ const {
   updateAdmin,
   deleteAdmin,
 } = require("../controllers/admin.controller");
-
 const {
-  loginAdmin,
   profileAdmin,
   deleteOwnAccount,
 } = require("../controllers/auth.controller");
 
-const {
-  getStates,
-  getCitiesByState,
-} = require("../controllers/meta.controller");
-
-// -------- Middleware --------
 const authMiddleware = require("../middleware/auth.middleware");
 const superAdminMiddleware = require("../middleware/superAdmin.middleware");
-//  ------------ AUTH ROUTES
 
-// -------------- Login admin
-router.post("/login", loginAdmin);
-
-// ---- Get logged-in admin profile
+// Profile OR Delete Own Account
 router.get("/profile", authMiddleware, profileAdmin);
-
-// ---- Delete own account
 router.delete("/delete-account", authMiddleware, deleteOwnAccount);
 
-// ----  META DATA ROUTES (States / Cities)
-router.get("/meta/states", getStates);
-router.get("/meta/cities/:stateId", getCitiesByState);
-
-// --------------------- ADMIN CRUD ROUTES
-
-// ---- Create new admin
+// Create admin
 router.post("/", authMiddleware, superAdminMiddleware, createAdmin);
 
-// ---- Get all admins
+// Get all admins
 router.get("/", authMiddleware, superAdminMiddleware, getAllAdmins);
 
-// ---- Get admin by id
+// Get admin by id
 router.get("/:id", authMiddleware, superAdminMiddleware, getAdminById);
 
-// ---- Update admin
+// Update admin
 router.put("/:id", authMiddleware, superAdminMiddleware, updateAdmin);
 
-// ---- Delete admin
+// Delete admin
 router.delete("/:id", authMiddleware, superAdminMiddleware, deleteAdmin);
 
 module.exports = router;
