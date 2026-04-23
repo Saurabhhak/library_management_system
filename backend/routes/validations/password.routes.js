6f519540dd15dbd0ac26576863f5782e68dff2dc
Saurabh <skkashap2328@email.com>
2026-04-23 12:59:46 +0530

    feat(auth): introduce step-based OTP verification flow for password reset- add send-otp and verify-otp endpoints- separate OTP logic into dedicated controller- refactor password reset into multi-step process- improve security with explicit OTP validation step- enhance modularity and reusability of auth system

--- backend/routes/validations/password.routes.js
+++ backend/routes/validations/password.routes.js
@@ -1,26 +1,11 @@
-const express = require("express");
-const router = express.Router();
+  const express = require("express");
+  const router = express.Router();
+  const {
+    forgotPassword,
+    resetPassword,
+  } = require("../../controllers/validations/password.controller");
 
-const {
-  forgotPassword,
-  resetPassword,
-} = require("../../controllers/validations/password.controller");
+  router.post("/forgot-password", forgotPassword);
+  router.post("/reset-password", resetPassword);
 
-const {
-  sendOtp,
-  verifyOtp,
-} = require("../../controllers/validations/otp.controller");
-// ================= PASSWORD RESET =================
-
-router.post("/forgot-password", forgotPassword);
-
-// Step 1 → Send OTP
-router.post("/send-otp", sendOtp);
-
-// Step 2 → Verify OTP
-router.post("/verify-otp", verifyOtp);
-
-// Step 3 → Reset Password
-router.post("/reset-password", resetPassword);
-
-module.exports = router;
+  module.exports = router;
