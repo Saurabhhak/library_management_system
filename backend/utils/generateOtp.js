31b7b625800542cc3ef5f91a202fb0fdb802d6ee
Saurabh <skkashap2328@email.com>
2026-04-23 13:00:56 +0530

    refactor(utils): make OTP generator return explicit for readability

--- backend/utils/generateOtp.js
+++ backend/utils/generateOtp.js
@@ -1,7 +1,6 @@
 const crypto = require("crypto");
 
-const generateOtp = (length = 6) => {
-  return crypto.randomInt(10 ** (length - 1), 10 ** length).toString();
-};
+const generateOtp = (length = 6) =>
+  crypto.randomInt(10 ** (length - 1), 10 ** length).toString();
 
 module.exports = generateOtp;
